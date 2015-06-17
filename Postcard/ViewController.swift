73560a10da3e7ae3b0489c354e11a968d71c67e5
Michael Fullerton <mfullert@tdsm8BF1G3.local>
2015-06-16 20:13:26 -0700

    added a comment to tedt commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //Adding a comment
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
